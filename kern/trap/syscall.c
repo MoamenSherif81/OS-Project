ae455e4ca56dedcbced62399e0f9a960a068b08a
Momen Kadry <kadry.momen6@proton.me>
2023-10-20 22:03:12 +0300

    syscalls [3] (untested)

--- kern/trap/syscall.c
+++ kern/trap/syscall.c
@@ -499,7 +499,7 @@ void* sys_sbrk(int increment)
 	 * 	2) New segment break should be aligned on page-boundary to avoid "No Man's Land" problem
 	 * 	3) As in real OS, allocate pages lazily. While sbrk moves the segment break, pages are not allocated
 	 * 		until the user program actually tries to access data in its heap (i.e. will be allocated via the fault handler).
-	 * 	4) Allocating additional pages for a process’ heap will fail if, for example, the free frames are exhausted
+	 * 	4) Allocating additional pages for a processï¿½ heap will fail if, for example, the free frames are exhausted
 	 * 		or the break exceed the limit of the dynamic allocator. If sys_sbrk fails, the net effect should
 	 * 		be that sys_sbrk returns (void*) -1 and that the segment break and the process heap are unaffected.
 	 * 		You might have to undo any operations you have done so far in this case.
@@ -712,6 +712,15 @@ uint32 syscall(uint32 syscallno, uint32 a1, uint32 a2, uint32 a3, uint32 a4, uin
 	case SYS_check_WS_list:
 		return sys_check_WS_list((uint32*)a1, (int)a2, (uint32)a3, (bool)a4);
 
+	case SYS_sbrk:
+		return;
+		
+	case SYS_free_user_mem:
+		return;
+	
+	case SYS_allocate_user_mem:
+		return;
+		
 	case NSYSCALLS:
 		return 	-E_INVAL;
 		break;
